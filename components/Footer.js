40c9143de5bdf6004cb14b440724687b938ca679
kp1153 <kamta_tiwari@yahoo.com>
2025-12-10 22:58:50 +0530

    force deploy

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     <footer className="bg-gray-900 text-gray-300 py-4">
       <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center text-sm">
         <p>
-          © 2025 विवेक त्यागी | संपर्क: 97170 44638 | ईमेल:        
+          © 2025 विवेक त्यागी | संपर्क: 97170 44638 | ईमेल: vivektyagi1153@rediffmail.com       
         </p>
         <p>
           वेबसाइट Developed by:{" "}
